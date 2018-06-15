8226806973b7df51e5e9ef145f9dca8118e08141
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-06-15 18:26:59 +0200

    Add tests for Mosaic layout

--- Tests/macOS/Helper.swift
+++ Tests/macOS/Helper.swift
@@ -43,5 +43,34 @@ class Helper {
 
     return (collectionView: collectionView, layout: layout)
   }
+
+  static func createVerticalMosaicLayout(dataSource: NSCollectionViewDataSource) -> (collectionView: CollectionView, layout: VerticalMosaicBlueprintLayout) {
+    let frame = CGRect(origin: .zero, size: CGSize(width: 200, height: 200))
+    let scrollView = NSScrollView()
+    let window = NSWindow()
+    window.setFrame(frame, display: true)
+    scrollView.frame = frame
+
+    let patterns = [
+      MosaicPattern(alignment: .left, direction: .vertical, amount: 2, multiplier: 0.5),
+      MosaicPattern(alignment: .left, direction: .horizontal, amount: 2, multiplier: 0.5),
+      MosaicPattern(alignment: .right, direction: .vertical, amount: 2, multiplier: 0.5)
+    ]
+
+    let layout = VerticalMosaicBlueprintLayout(
+      minimumInteritemSpacing: 2,
+      minimumLineSpacing: 2,
+      sectionInset: EdgeInsets(top: 2, left: 2, bottom: 2, right: 2),
+      patterns: patterns)
+    layout.itemSize = CGSize(width: 50, height: 50)
+    layout.estimatedItemSize = CGSize(width: 50, height: 50)
+    let collectionView = CollectionView(frame: frame, collectionViewLayout: layout)
+    collectionView.register(MockCollectionViewItem.self, forItemWithIdentifier: NSUserInterfaceItemIdentifier.init(rawValue: "cell"))
+    collectionView.dataSource = dataSource
+    scrollView.documentView = collectionView
+    window.contentView = scrollView
+
+    return (collectionView: collectionView, layout: layout)
+  }
 }
 

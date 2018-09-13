4905edaf5679718b50e76597a84fc7dc298da847
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-09-13 20:21:02 +0200

    Update Vertical Mosaic tests

--- Tests/Shared/VerticalMosaicBlueprintLayoutTests.swift
+++ Tests/Shared/VerticalMosaicBlueprintLayoutTests.swift
@@ -18,25 +18,25 @@ class VerticalMosaicBlueprintLayoutTests: XCTestCase {
 
     XCTAssertEqual(layout.cachedAttributes[0].count, 10)
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 0, section: 0))?.frame,
-                   CGRect(origin: .init(x: 2, y: 2), size: CGSize(width: 98, height: 23)))
+                   CGRect(origin: .init(x: 2, y: 2), size: CGSize(width: 97, height: 23)))
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 1, section: 0))?.frame,
-                   CGRect(origin: .init(x: 102, y: 2), size: CGSize(width: 94, height: 10)))
+                   CGRect(origin: .init(x: 101, y: 2), size: CGSize(width: 97, height: 10)))
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 2, section: 0))?.frame,
-                   CGRect(origin: .init(x: 102, y: 14), size: CGSize(width: 94, height: 10)))
+                   CGRect(origin: .init(x: 101, y: 14), size: CGSize(width: 97, height: 10)))
 
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 3, section: 0))?.frame,
-                   CGRect(origin: .init(x: 2, y: 27), size: CGSize(width: 98, height: 23)))
+                   CGRect(origin: .init(x: 2, y: 27), size: CGSize(width: 97, height: 23)))
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 4, section: 0))?.frame,
-                   CGRect(origin: .init(x: 102, y: 27), size: CGSize(width: 47, height: 23)))
+                   CGRect(origin: .init(x: 101, y: 27), size: CGSize(width: 47.5, height: 23)))
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 5, section: 0))?.frame,
-                   CGRect(origin: .init(x: 151, y: 27), size: CGSize(width: 47, height: 23)))
+                   CGRect(origin: .init(x: 150.5, y: 27), size: CGSize(width: 47.5, height: 23)))
 
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 6, section: 0))?.frame,
-                   CGRect(origin: .init(x: 100, y: 52), size: CGSize(width: 98, height: 23)))
+                   CGRect(origin: .init(x: 101, y: 52), size: CGSize(width: 97, height: 23)))
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 7, section: 0))?.frame,
-                   CGRect(origin: .init(x: 2, y: 52), size: CGSize(width: 94, height: 10)))
+                   CGRect(origin: .init(x: 2, y: 52), size: CGSize(width: 97, height: 10)))
     XCTAssertEqual(layout.layoutAttributesForItem(at: IndexPath(item: 8, section: 0))?.frame,
-                   CGRect(origin: .init(x: 2, y: 64), size: CGSize(width: 94, height: 10)))
+                   CGRect(origin: .init(x: 2, y: 64), size: CGSize(width: 97, height: 10)))
 
 
   }

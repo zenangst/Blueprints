1f30350c983f488c583f5a0595a50e116d169a09
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-12-28 13:00:23 +0100

    Fix Example project

--- Example-iOS/Scenes/LayoutExampleSceneViewController+VerticalMosaicBlueprintLayout.swift
+++ Example-iOS/Scenes/LayoutExampleSceneViewController+VerticalMosaicBlueprintLayout.swift
@@ -4,35 +4,34 @@ import UIKit
 extension LayoutExampleSceneViewController {
 
     func configureMosaicLayout() {
-        let mosaicBlueprintLayout = VerticalMosaicBlueprintLayout(
-            itemSize: CGSize.init(width: 50,
-                                  height: 400),
-            minimumInteritemSpacing: minimumInteritemSpacing,
-            minimumLineSpacing: minimumLineSpacing,
-            sectionInset: sectionInsets,
-            patterns: [
-                MosaicPattern(alignment: .left,
-                              direction: .vertical,
-                              amount: 2,
-                              multiplier: 0.6),
-                MosaicPattern(alignment: .left,
-                              direction: .horizontal,
-                              amount: 2,
-                              multiplier: 0.33),
-                MosaicPattern(alignment: .left,
-                              direction: .vertical,
-                              amount: 1,
-                              multiplier: 0.5),
-                MosaicPattern(alignment: .left,
-                              direction: .vertical,
-                              amount: 1,
-                              multiplier: 0.5)
-            ])
-
-        UIView.animate(withDuration: 0.5) { [weak self] in
-            self?.layoutExampleCollectionView.collectionViewLayout = mosaicBlueprintLayout
-            self?.view.setNeedsLayout()
-            self?.view.layoutIfNeeded()
-        }
-    }
+      let mosaicBlueprintLayout = VerticalMosaicBlueprintLayout(
+        patternHeight: 400,
+        minimumInteritemSpacing: minimumInteritemSpacing,
+        minimumLineSpacing: minimumLineSpacing,
+        sectionInset: sectionInsets,
+        patterns: [
+          MosaicPattern(alignment: .left,
+                        direction: .vertical,
+                        amount: 2,
+                        multiplier: 0.6),
+          MosaicPattern(alignment: .left,
+                        direction: .horizontal,
+                        amount: 2,
+                        multiplier: 0.33),
+          MosaicPattern(alignment: .left,
+                        direction: .vertical,
+                        amount: 1,
+                        multiplier: 0.5),
+          MosaicPattern(alignment: .left,
+                        direction: .vertical,
+                        amount: 1,
+                        multiplier: 0.5)
+        ])
+      
+      UIView.animate(withDuration: 0.5) { [weak self] in
+        self?.layoutExampleCollectionView.collectionViewLayout = mosaicBlueprintLayout
+        self?.view.setNeedsLayout()
+        self?.view.layoutIfNeeded()
+      }
+  }
 }

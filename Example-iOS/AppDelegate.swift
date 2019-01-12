9fa981b31417f8f4d8b92f3037742252615152d8
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-01-12 13:29:35 +0100

    Load injection bundle for faster debugging

--- Example-iOS/AppDelegate.swift
+++ Example-iOS/AppDelegate.swift
@@ -6,6 +6,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        #if DEBUG
+        Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/iOSInjection10.bundle")?.load()
+        #endif
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .groupTableViewBackground
         window?.rootViewController = RootViewController()

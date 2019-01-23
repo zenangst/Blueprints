2fe17f08ede1d28c6524cae7de0816c462d74b3a
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-01-23 10:12:04 +0100

    Add basic header support for mosiac layout (#75)
    * Add basic header support for mosiac layout
    
    * Remove reference to stickyFooter
    
    * Fix faulty nextY
    
    * Fix failing test
    
    * Remove references to stickyHeader as it is not implemented
    
    * Adding headerReferenceSize to example project. (#76)
    
    Shows the basic implementation for headers in the mosaic layout.

--- Example-iOS/Scenes/LayoutExampleSceneViewController+VerticalMosaicBlueprintLayout.swift
+++ Example-iOS/Scenes/LayoutExampleSceneViewController+VerticalMosaicBlueprintLayout.swift
@@ -27,6 +27,9 @@ extension LayoutExampleSceneViewController {
                         amount: 1,
                         multiplier: 0.5)
         ])
+        
+      let titleCollectionReusableViewSize = CGSize(width: view.bounds.width, height: 61)
+      mosaicBlueprintLayout.headerReferenceSize = titleCollectionReusableViewSize
       
       UIView.animate(withDuration: 0.5) { [weak self] in
         self?.layoutExampleCollectionView.collectionViewLayout = mosaicBlueprintLayout

21358edd073d282ac2205d5d75aadf191b26df2d
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-02-09 15:54:22 +0100

    Clean InvalidationContext

--- Sources/Shared/Core/InvalidationContext.swift
+++ Sources/Shared/Core/InvalidationContext.swift
@@ -5,13 +5,7 @@ import UIKit
 #endif
 
 class InvalidationContext: FlowLayoutInvalidationContext {
-  var invalidateSectionHeadersFooters = [IndexPath]()
   var shouldInvalidateEverything = true
-
-  override var invalidatedItemIndexPaths: Set<IndexPath>? {
-    return !invalidateSectionHeadersFooters.isEmpty ? Set(invalidateSectionHeadersFooters) : nil
-  }
-
   override var invalidateEverything: Bool {
     return shouldInvalidateEverything
   }

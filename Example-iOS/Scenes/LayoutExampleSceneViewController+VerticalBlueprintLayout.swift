f445dcbf0f9d0b32fc1e79ab7d526a3fad240c60
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-08-05 12:37:40 +0200

    Fix initial rendering bug in example

--- Example-iOS/Scenes/LayoutExampleSceneViewController+VerticalBlueprintLayout.swift
+++ Example-iOS/Scenes/LayoutExampleSceneViewController+VerticalBlueprintLayout.swift
@@ -3,27 +3,30 @@ import UIKit
 
 extension LayoutExampleSceneViewController {
 
-    func configureVerticalLayout() {
-        let verticalBlueprintLayout = VerticalBlueprintLayout(
-          itemsPerRow: itemsPerRow,
-          height: 95,
-          minimumInteritemSpacing: minimumInteritemSpacing,
-          minimumLineSpacing: minimumLineSpacing,
-          sectionInset: sectionInsets,
-          stickyHeaders: true,
-          stickyFooters: true
-        )
+  func configureVerticalLayout() {
+    let verticalBlueprintLayout = VerticalBlueprintLayout(
+      itemsPerRow: itemsPerRow,
+      height: 95,
+      minimumInteritemSpacing: minimumInteritemSpacing,
+      minimumLineSpacing: minimumLineSpacing,
+      sectionInset: sectionInsets,
+      stickyHeaders: true,
+      stickyFooters: true
+    )
 
-      verticalBlueprintLayout.estimatedItemSize = .init(width: 100, height: 250)
+    verticalBlueprintLayout.estimatedItemSize = .init(width: 100, height: 250)
 
-        let titleCollectionReusableViewSize = CGSize(width: view.bounds.width, height: 61)
-        verticalBlueprintLayout.headerReferenceSize = titleCollectionReusableViewSize
-        verticalBlueprintLayout.footerReferenceSize = titleCollectionReusableViewSize
+    let titleCollectionReusableViewSize = CGSize(width: view.bounds.width, height: 61)
+    verticalBlueprintLayout.headerReferenceSize = titleCollectionReusableViewSize
+    verticalBlueprintLayout.footerReferenceSize = titleCollectionReusableViewSize
 
-        UIView.animate(withDuration: 0.5) { [weak self] in
-            self?.layoutExampleCollectionView.collectionViewLayout = verticalBlueprintLayout
-            self?.view.setNeedsLayout()
-            self?.view.layoutIfNeeded()
-        }
+    UIView.animate(withDuration: 0.5) { [weak self] in
+      self?.layoutExampleCollectionView.collectionViewLayout = verticalBlueprintLayout
+
+      if self?.layoutExampleCollectionView.contentSize.height != 0 {
+        self?.view.setNeedsLayout()
+        self?.view.layoutIfNeeded()
+      }
     }
+  }
 }

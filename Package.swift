4560b1779e4eca9d9c7e95ed335588dfbc4d7a27
Dal Rupnik <legoless@gmail.com>
2020-04-18 11:44:36 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Blueprints",
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9_2),
+        .macOS(.v10_11)
+    ]
+    products: [
+        .library(name: "Blueprints", targets: ["Blueprints"]),
+    ],
+    targets: [
+         .target(
+            name: "Blueprints",
+            path: "Sources",
+            exclude: [
+                "Sources/macOS"
+            ]
+         )
+    ]
+)
